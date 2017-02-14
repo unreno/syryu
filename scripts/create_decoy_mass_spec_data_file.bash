ffd170e7b405dfa1d1455acb21b52458c83e730f
Jake <github@jakewendt.com>
2017-02-14 13:41:23 -0800

    Fine tuning.

--- scripts/create_decoy_mass_spec_data_file.bash
+++ scripts/create_decoy_mass_spec_data_file.bash
@@ -20,12 +20,13 @@ function usage(){
   echo "$script <OPTIONS> mgf_file(s)"
   echo
 	echo "Options:"
-	echo "	--min_shift ........ Minimum value of shift range"
-	echo "	--max_shift ........ Maximum value of shift range"
-	echo "	--exclusion_min .... Do not shift values ABOVE this"
-	echo "	--exclusion_max .... Do not shift values BELOW this"
-	echo "	--number_of_peaks .. NOT IMPLEMENTED YET"
-	echo "	--percent_of_peaks . PERCENT CHANCE PEAK WILL BE SHIFTED (not sure about this)"
+	echo "	--min_shift INTEGER......... Minimum value of shift range"
+	echo "	--max_shift INTEGER......... Maximum value of shift range"
+	echo "	--exclusion_min INTEGER..... Do not shift values ABOVE this"
+	echo "	--exclusion_max INTEGER..... Do not shift values BELOW this"
+	echo "	--number_of_peaks .......... NOT IMPLEMENTED YET"
+	echo "	--percent_of_peaks INTEGER.. PERCENT CHANCE PEAK WILL BE SHIFTED (not sure about this)"
+	echo "	--verbose .................. NO VALUE, just boolean flag"
 	echo
 	echo "Default option values:"
 	echo "	--min_shift ........ ${min_shift}"
@@ -34,6 +35,7 @@ function usage(){
 	echo "	--exclusion_max .... ${exclusion_max}"
 	echo "	--number_of_peaks .. ${number_of_peaks} NOT YET"
 	echo "	--percent_of_peaks . ${percent_of_peaks}"
+	echo "	--verbose .......... ${verbose}"
 	echo
 	echo "Notes:"
 	echo "	number and percent peaks are mutually exclusive. Setting one deletes the other."
@@ -51,8 +53,6 @@ function usage(){
 
 while [ $# -ne 0 ] ; do
 	case $1 in
-#					-b|--b*)
-#						shift; blast=$1; shift ;;
 		--min_shift)
 			shift; min_shift=$1; shift;;
 		--max_shift)
@@ -61,10 +61,10 @@ while [ $# -ne 0 ] ; do
 			shift; exclusion_min=$1; shift;;
 		--exclusion_max)
 			shift; exclusion_max=$1; shift;;
-		--percent_of_peaks)
+		--percent*)
 			shift; percent_of_peaks=$1; shift;;
-#number_of_peaks=
-#percent_of_peaks=30
+		-v|--v*)
+			verbose=true; shift;;
 		-*)
 			echo ; echo "Unexpected args from: ${*}"; usage ;;
 		*)
@@ -85,6 +85,7 @@ awk \
 	-v exclusion_min=$exclusion_min \
 	-v exclusion_max=$exclusion_max \
 	-v percent_of_peaks=$percent_of_peaks \
+	-v verbose=$verbose \
 '
 BEGIN{
 	outfile=sprintf("%s.DECOY.mgf",infile);
@@ -94,35 +95,38 @@ BEGIN{
 ( /^PEPMASS/ ){
 	split($1,s,"=");
 	pepmass = s[2];
-	printf "PEPMASS = %s\n", pepmass;
+	if( verbose ) printf( "PEPMASS = %s\n", pepmass );
 }
 ( /^CHARGE/ ){
 	split($1,s,"=");
 	charge = s[2];
-	printf "CHARGE = %s\n", charge;
+	if( verbose ) printf( "CHARGE = %s\n", charge );
 	max_mass = pepmass * charge;
-	printf "MAX MASS = %s\n", max_mass;
+	if( verbose ) printf( "MAX MASS = %s\n", max_mass );
 }
 ( /^[[:alpha:]]/ ){ print >> outfile; }
 ( /^[[:digit:]]/ ){
-	print $1
-	print exclusion_min, exclusion_max
+	if( verbose ) print $1
+	if( verbose ) print "Exclusion range " exclusion_min " - " exclusion_max
 	if( ( $1 < exclusion_min ) || ( $1 > exclusion_max ) ){
-		print "... is not in exclusion range ...";
-		if( int( rand() * 100 ) <= percent_of_peaks ) {
-			print "... has been randomly selected to shift ...";
-			random_shift = min_shift + int(rand() * ( max_shift - min_shift + 1 ) );
+		if( verbose ) print "... is not in exclusion range ...";
+		num = int( rand() * 100 )
+		if( num < percent_of_peaks ) {
+			if( verbose ) print "... has been randomly ( " num " < " percent_of_peaks " ) selected to shift ...";
+			if( verbose ) print "... between " min_shift " and " max_shift " ...";
+			random_shift = min_shift + int(10 * rand() * ( max_shift - min_shift + 1 ) ) / 10;
+			if( verbose ) print "... by " random_shift " ...";
 			new_mass = $1 + random_shift;
-			print "... to new mass " new_mass " ...";
+			if( verbose ) print "... to new mass " new_mass " ...";
 			if( ( new_mass > 0 ) && ( new_mass < max_mass ) ){
-				print "... that is between 0 and " max_mass " ...";
+				if( verbose ) print "... that is between 0 and " max_mass " ...";
 				$1 = new_mass;
 			} else {
-				print "BUT IS NOT BETWEEN 0 and " max_mass;
+				if( verbose ) print "BUT IS NOT BETWEEN 0 and " max_mass;
 			}
 		}
 	} else {
-		print "... BUT IS IN EXCLUSION RANGE ...";
+		if( verbose ) print "... BUT IS IN EXCLUSION RANGE ...";
 	}
 	print $1, $2 >> outfile
 }' $1

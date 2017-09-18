ad9624f155355b5a7a49fc05325f95db0ef35882
Jake <github@jakewendt.com>
2017-09-18 10:28:29 -0700

    Added echos

--- scripts/MSGFPlus.bash
+++ scripts/MSGFPlus.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Calling ..."
+echo java -Xmx3500M -jar $(cygpath -w /cygdrive/c/ryulab/MSGFPlus/MSGFPlus.jar) "$@"
+echo
+
 java -Xmx3500M -jar $(cygpath -w /cygdrive/c/ryulab/MSGFPlus/MSGFPlus.jar) "$@"
--- scripts/MzIDToTsv.bash
+++ scripts/MzIDToTsv.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Calling ..."
+echo java -cp $(cygpath -w /cygdrive/c/ryulab/MSGFPlus/MSGFPlus.jar) edu.ucsd.msjava.ui.MzIDToTsv "$@"
+echo
+
 java -cp $(cygpath -w /cygdrive/c/ryulab/MSGFPlus/MSGFPlus.jar) edu.ucsd.msjava.ui.MzIDToTsv "$@"

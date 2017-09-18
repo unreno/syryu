41853a2a42964be38483bc8c4701dd70a140ae66
Jake <github@jakewendt.com>
2017-09-18 10:25:34 -0700

    Trying cygdrive call

--- scripts/MzIDToTsv.bash
+++ scripts/MzIDToTsv.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp C:\ryulab\MSGFPlus\MSGFPlus.jar edu.ucsd.msjava.ui.MzIDToTsv "$@"
+java -cp $(cygpath /cygdrive/c/ryulab/MSGFPlus/MSGFPlus.jar) edu.ucsd.msjava.ui.MzIDToTsv "$@"

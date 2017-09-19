d49ae798581207c11c6fa84d0c45edb133fc923b
Jake <github@jakewendt.com>
2017-09-19 10:49:16 -0700

    Deving mgf file chopper.

---
+++ scripts/mgf_chopper.bash
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+
+script=`basename $0`
+
+#	Defaults:
+max=5
+
+function usage(){
+	echo
+	echo "Splits mgf file limiting the number of scans."
+	echo "Output goes to filename with number inserted between base and extension" 
+	echo
+	echo "Usage:"
+	echo
+	echo "$script <OPTIONS> mgf_files(s)"
+	echo
+	echo "Options:"
+	echo "	--max INTEGER ......... Maximum number of scans per file"
+#	echo "	--verbose .................. NO VALUE, just boolean flag"
+	echo
+	echo "Default option values:"
+	echo "	--max ........ ${max}"
+#	echo "	--verbose .......... ${verbose}"
+	echo
+	echo "Examples:"
+	echo "	$script file.mgf"
+	echo "	$script --max 10 file.mgf"
+	echo
+	echo
+	exit 1
+}
+
+while [ $# -ne 0 ] ; do
+	case $1 in
+		-m|--m*)
+			shift; max=$1; shift;;
+#		-v|--v*)
+#			verbose=true; shift;;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*)
+			break;;
+	esac
+done
+
+#	Basically, this is TRUE AND DO ...
+[ $# -eq 0 ] && usage
+
+
+while [ $# -ne 0 ] ; do
+	echo $1
+
+	base=${1%.*}		#	drop the extension
+	ext=${1##*.}		#	drop the base
+
+	echo $base
+	echo $ext
+
+
+
+#	awk 
+
+	echo "THIS IS STILL UNDEVELOPED."
+
+
+
+
+
+
+
+	shift
+done

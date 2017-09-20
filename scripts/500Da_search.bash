7a8300853fcd376a0aae8795b03345776a2db2a4
Jake <github@jakewendt.com>
2017-09-19 17:18:26 -0700

    Cleanup and refining scripts.

--- scripts/500Da_search.bash
+++ scripts/500Da_search.bash
@@ -62,48 +62,40 @@ esac
 while [ $# -ne 0 ] ; do
 	echo $1
 
-#	base=${1%.*}		#	drop the extension
-#	echo $base
-
-
-#	
-#	<RUN 1: 500Da search>
-#	I uploaded 3 mgf files in the RyuLab storage (PTM_blind).
-#	 
-#	1
-#	Break mgf files into smaller files (5 scans per file)
-#	 
-#	ex) The beginning of scan is "BEGIN IONS" and the end of scan is "END IONS".
-
-#	split_mgf_dir=$(mgf_splitter.bash --max 5 $1)
 	split_mgf_dir=$(mgf_splitter.bash --max 10 $1)
-
-#	 
-#	 
-#	2
-#	Run MSGFPlus.jar with 10000M
-#	Use Mods2.txt instead of Mods.txt for this search.
-#	 
 
 	for file in $( find $split_mgf_dir -name \*mgf	); do
 		
 		base=${file%.*}		#	drop the extension
 
-#		cmd="java -Xmx10G -jar $MSGFPlus -s $file -o $base.mzid -d uniprot_reviewed_April2016.fasta -t 500Da -ti 0,1 -inst 1 -protocol 1 -mod Mods2.txt -n 10 -addFeatures 1"
-		cmd="java -Xmx16G -jar $MSGFPlus -s $file -o $base.mzid -d uniprot_reviewed_April2016.fasta -t 500Da -ti 0,1 -inst 1 -protocol 1 -mod Mods2.txt -n 10 -addFeatures 1"
+		cmd="java -Xmx16G -jar $MSGFPlus -s $file -o $base.500Da.mzid -d uniprot_reviewed_April2016.fasta -t 500Da -ti 0,1 -inst 1 -protocol 1 -mod Mods2.txt -n 10 -addFeatures 1"
 		echo $cmd
 		$cmd
 
-		cmd="java -cp $MSGFPlus edu.ucsd.msjava.ui.MzIDToTsv -i $base.mzid -showFormula 1"
+		cmd="java -cp $MSGFPlus edu.ucsd.msjava.ui.MzIDToTsv -i $base.500Da.mzid -showFormula 1"
 		echo $cmd
 		$cmd
 
 	done
-#	 
-#	 
-#	4
-#	Merge resulting tsv files
-#	 
+
+
+
+
+
+
+	base=${1%.*}		#	drop the extension
+#	echo $base
+
+	#	Merge resulting tsv files
+
+	first_tsv=$(ls -1 $split_mgf_dir/*tsv | head -1 )
+	head -1 $first_tsv > $base.500Da.tsv
+	tail --quiet --lines=+2 $split_mgf_dir/*tsv >> $base.500Da.tsv
+
+
+#	/bin/rm -rf $split_mgf_dir
+
+
 
 	shift
 done

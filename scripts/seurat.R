ab86e8a08904820563b7fe8279f0714dc45e8167
Jake <github@jakewendt.com>
2018-01-02 09:06:38 -0800

    R script failed. Modified. Trying again.

--- scripts/seurat.R
+++ scripts/seurat.R
@@ -33,7 +33,26 @@ print("Loading data from error_detected.dge.txt.gz")
 # load data
 ds.data <- read.table("error_detected.dge.txt.gz",row.names=1,header=T)
 #ds <- CreateSeuratObject(raw.data = ds.data)
-ds <- CreateSeuratObject(raw.data = ds.data, min.cells = 3,  min.genes = 200, is.expr=1)
+
+
+#ds <- CreateSeuratObject(raw.data = ds.data, min.cells = 3,  min.genes = 200, is.expr=1)
+#
+#	Error in if (!nreplace) return(x) : missing value where TRUE/FALSE needed
+#	Calls: CreateSeuratObject ... suppressMessages -> withCallingHandlers -> [<- -> [<-.data.frame
+#	In addition: Warning message:
+#	In sum(i, na.rm = TRUE) : integer overflow - use sum(as.numeric(.))
+#	Execution halted
+#
+#	Maybe, we need to remove "is.expr=1"? 
+#
+#	ds <- CreateSeuratObject(raw.data = ds.data, min.cells = 3,  min.genes = 200)
+#
+#	Or filter more data at DigitalExpression stage in dropseq (i.e. MIN_NUM_GENES_PER_CELL=2000 instead of 100)?
+#	Others kept only 0.1% of their data for Seurat.
+#
+#	Trying removing is.expr=1
+
+ds <- CreateSeuratObject(raw.data = ds.data, min.cells = 3,  min.genes = 200)
 ds <- NormalizeData(object = ds)
 ds <- ScaleData(object = ds)
 ds <- FindVariableGenes(object = ds, do.plot = FALSE)

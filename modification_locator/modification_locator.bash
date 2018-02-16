1d0cce987e00b03b3ae93afe2ab62258a1090c37
Jake <github@jakewendt.com>
2018-02-15 16:42:13 -0800

    Deving modification locator

--- modification_locator/modification_locator.bash
+++ modification_locator/modification_locator.bash
@@ -1,39 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
 
-
-#	I need a software (Modification Locator) that locates peptide modification in protein.
-#
-#	For evidence file, please use “Modified sequence” column and “Leading razor protein” column.
-
-
-#==> evidence.txt <==
-#Sequence	Length	Modifications	Modified sequence	Oxidation (M) Probabilities	Phospho (STY) Probabilities	Oxidation (M) Score Diffs	Phospho (STY) Score Diffs	Acetyl (Protein N-term)	Oxidation (M)	Phospho (STY)	Missed cleavages	Proteins	Leading proteins	Leading razor protein	Type	Raw file	Fraction	Experiment	MS/MS m/z	Charge	m/z	Mass	Resolution	Uncalibrated - Calibrated m/z [ppm]	Uncalibrated - Calibrated m/z [Da]	Mass Error [ppm]	Mass Error [Da]	Uncalibrated Mass Error [ppm]	Uncalibrated Mass Error [Da]	Max intensity m/z 0	Retention time	Retention length	Calibrated retention time	Calibrated retention time start	Calibrated retention time finish	Retention time calibration	Match time difference	Match m/z difference	Match q-value	Match score	Number of data points	Number of scans	Number of isotopic peaks	PIF	Fraction of total spectrum	Base peak fraction	PEP	MS/MS Count	MS/MS Scan Number	Score	Delta score	Combinatorics	Intensity	Reverse	Potential contaminant	id	Protein group IDs	Peptide ID	Mod. peptide ID	MS/MS IDs	Best MS/MS	AIF MS/MS IDs	Oxidation (M) site IDs	Phospho (STY) site IDs
-
-#	head evidence.txt | awk -F"\t" '{print $4,$15}'
-#Modified sequence Leading razor protein
-#_AAAAAAAAAAAAAAAGAGAGAK_ tr|Q53ZR1|Q53ZR1_HUMAN
-#_AAAAAAAAAAAAAAAGAGAGAK_ tr|Q53ZR1|Q53ZR1_HUMAN
-#_AAAAAAAAAAAAAAAGAGAGAK_ tr|Q53ZR1|Q53ZR1_HUMAN
-#_AAAAAAAAAAAAAAAGAGAGAK_ tr|Q53ZR1|Q53ZR1_HUMAN
-#_AAAAAAAAAAAATGTEAGPGTAGGSENGSEVAAQPAGLSGPAEVGPGAVGER_ sp|O60341|KDM1A_HUMAN
-#_AAAAAAAAAAAATGTEAGPGTAGGSENGSEVAAQPAGLSGPAEVGPGAVGER_ sp|O60341|KDM1A_HUMAN
-#_AAAAAAAAAAAATGTEAGPGTAGGSENGSEVAAQPAGLSGPAEVGPGAVGER_ sp|O60341|KDM1A_HUMAN
-#_AAAAAAAAAAAATGTEAGPGTAGGSENGSEVAAQPAGLSGPAEVGPGAVGER_ sp|O60341|KDM1A_HUMAN
-#_AAAAAAAAAAAATGTEAGPGTAGGSENGSEVAAQPAGLSGPAEVGPGAVGER_ sp|O60341|KDM1A_HUMAN
-
-#
-#	I uploaded “evidence” file and “PeptideMatach” software in Azure Modification Locator folder. I uploaded fasta file which contains protein sequences in “Blob container”. (I failed to upload it to the same folder.)
-#
-#	 
-#
-#	I want two output files contains:
-#
-#	The first output file (name it as MatchedModification.txt) contains 1) Modified sequence, 2) Leading razor protein, 3) Start position, 4) End position, 5) STY position, 6) STY modification position, 7) M position, and 8) M modification position. Please give users option that they can choose their amino acids of interest. In this example, users chose “STY”and “M”.
-#	For the given “Modified sequence”, please fine the corresponding “Leading razor protein” in fasta file.
-#
-#	Then, find their start/end position of sequence in protein. The first position of protein starts with 1 instead of 0.
-#
 #	(I uploaded that codes (PeptideMatch java codes) that does this. It was previously published by another group. If you like, you can modify these codes or re-write it in your language you prefer.)
 #
 #	                If “Modified sequence” contains S, T, or Y, then please look for the location of either S, T, or Y in protein and record it. If there are more than one of these, then record all separated by comma.
@@ -87,7 +55,7 @@
 #
 #	_ASSST[80]TY_\t       PROTEIN1\t     3\t      9\t    4;5;6;7;8;9\t    7\t        \t      \t
 #
-#	_SSS[80]T[80]_\t      PROTEIN1\t     4\t      7\t    4;5;6;7\t        4;7\t      \t      \t
+#	_SSS[80]T[80]_\t      PROTEIN1\t     4\t      7\t    4;5;6;7\t        6;7\t      \t      \t
 #
 #	 
 #
@@ -95,13 +63,10 @@
 #
 #	Leading razor protein\t AA\t AA location\t Sequences with Modified AA\t Sequences with Unmodified AA\t
 #
-#	PROTEIN1\t       S\t    4\t     \t      _ASSST[80]TY_;_SSS[80]T[80]_
-#
-#	PROTEIN1\t       S\t    5\t     \t      _ASSST[80]TY_;_SSS[80]T[80]_
-#
-#	PROTEIN1\t       S\t    6\t     \t      _SSS[80]T[80]_\t                 _ASSST[80]TY_\t
-#
-#	PROTEIN1\t       T\t    7\t\    \t      _ASSST[80]TY_; _SSS[80]T[80]_\t                   \t                                                            
+#	PROTEIN1\t               S\t           4\t     \t                       _ASSST[80]TY_;_SSS[80]T[80]_
+#	PROTEIN1\t               S\t           5\t     \t                       _ASSST[80]TY_;_SSS[80]T[80]_
+#	PROTEIN1\t               S\t           6\t     _SSS[80]T[80]_\t         _ASSST[80]TY_
+#	PROTEIN1\t               T\t           7\t\    _ASSST[80]TY_; _SSS[80]T[80]_\t
 #
 #	…
 #
@@ -125,12 +90,166 @@
 
 #	Select only those matching the requested amino acids.
 
-#	[[ -f evidence.STYM.txt ]] || awk -F"\t" '( $4 ~ /[STYM]/ ){print $4,$15}' evidence.txt | sort -u > evidence.STYM.txt
+#	[[ -f evidence.STYM.txt ]] || tail -n +2 evidence.txt | awk -F"\t" '( $4 ~ /[STYM]/ ){print $4,$15}' | sort -u > evidence.STYM.txt
 
+#	for each modified sequence;
+#		remember its protein
+#		find position of modified sequence and protein in protein database
+#		find given amino acid positions
+#		find given modified amino acid positions
+#			
 
 #	This will likely be better done in something like ruby.
 
 
 
 
+
+
+script=$( basename $0 )
+
+#	Defaults:
+
+function usage(){
+	echo
+#	echo "Wrapper around calling Drop-seq_alignment.sh"
+	echo
+	echo "Usage:"
+	echo
+	echo "$script <OPTIONS>"
+	echo
+	echo "Options:"
+	echo "  --amino_acids COMMA SEPARATED TEXT"
+	echo "  --evidence TEXT TSV FILE"
+	echo "  --protein FASTA FILE"
+#	echo "	--estimated-num-cells (-n) INTEGER : "
+#	echo "	--genomedir (-g) STRING : Directory of STAR genome directory"
+#	echo "	--referencefasta (-r) STRING : Reference fasta of the Drop-seq reference metadata bundle"
+#	echo "	--max INTEGER ......... Maximum number of scans per file"
+#	echo "	--verbose .................. NO VALUE, just boolean flag"
+#	echo
+#	echo "Default option values:"
+#	echo "	--estimated-num-cells ... ${num_cells}"
+#	echo "	--genomedir ............. ${genomedir}"
+#	echo "	--referencefasta ........ ${referencefasta}"
+#	echo "	--max ........ ${max}"
+#	echo "	--verbose .......... ${verbose}"
+	echo
+	echo "Examples:"
+#	echo "	$script file.bam"
+#	echo "	$script --max 10 file.mgf"
+	echo
+	echo
+	exit 1
+}
+
+while [ $# -ne 0 ] ; do
+	case $1 in
+		-a|--amino_acids)
+			shift; amino_acids=$1; shift;;
+		-e|--evidence)
+			shift; evidence=$1; shift;;
+		-p|--protein)
+			shift; protein_fasta=$1; shift;;
+#		-n|--estimated-num-cells)
+#			shift; num_cells=$1; shift;;
+#		-g|--genomedir)
+#			shift; genomedir=$1; shift;;
+#		-r|--referencefasta)
+#			shift; referencefasta=$1; shift;;
+#		-m|--m*)
+#			shift; max=$1; shift;;
+#		-v|--v*)
+#			verbose=true; shift;;
+		-*)
+			echo ; echo "Unexpected args from: ${*}"; usage ;;
+		*)
+			break;;
+	esac
+done
+
+#	Basically, this is TRUE AND DO ...
+#[ $# -eq 0 ] && usage
+[[ -z $amino_acids ]] && ( echo -e "\nAmino Acids Required" && usage )
+[[ -f $evidence ]] || ( echo -e "\nEvidence file Required" && usage )
+[[ -f $protein_fasta ]] || ( echo -e "\nProtein fasta file Required" && usage )
+
+protein_base_name=${protein_fasta%%.*}
+echo $protein_base_name
+
+if [[ ! -d $protein_base_name ]] ; then
+	echo "Creating protein database."
+	java -jar PeptideMatchCMD_1.0.jar -a index -d ${protein_fasta} -i ${protein_base_name}
+fi
+
+
+acids=$(echo $amino_acids | sed 's/[^[:alpha:]]//g' )
+echo $acids
+
+[[ -f evidence.${acids}.txt ]] || tail -n +2 evidence.txt | awk -F"\t" '( $4 ~ /['$acids']/ ){print $4"\t"$15}' | sort -u > evidence.${acids}.txt
+
+
+
+echo -e -n "Modified sequence\tLeading razor protein\tStart position\tEnd position" > MatchedModification.txt
+for aa in $( echo ${amino_acids} | awk -F, '{ for(i=1;i<=NF;i++) print $i }' ) ; do
+	echo -e -n "\t${aa} position\t${aa} modification position" >> MatchedModification.txt
+done
+echo >> MatchedModification.txt
+
+
+while read line; do
+#	i=$[ $i + 1 ]
+#	if [ $i -gt 30 ] ; then break ;fi
+
+  echo $line
+	sequence=${line%	*}
+	echo $sequence
+	protein=${line#*	}
+	echo $protein
+
+	cleaned_sequence=$( echo ${sequence} | sed -e 's/_//g' -e 's/([[:alpha:]]*)//g' )
+	echo $cleaned_sequence
+
+	out=${protein_base_name}.${cleaned_sequence}.txt
+
+	[[ -f ${out} ]] ||  java -jar PeptideMatchCMD_1.0.jar -a query -i ${protein_base_name} -q ${cleaned_sequence} -o ${out}
+
+	number_matches_in_protein=$( cat ${out} | grep ${protein} | wc -l )
+	#	I suppose its possible that sequence is repeated in a given protein.
+	#	Not sure what to do if it is.
+
+	[[ ${number_matches_in_protein} -eq 0 ]] && ( echo "None of the matches match the xpected protein."; exit )
+
+	match_start=$( cat ${out} | grep ${protein} | awk '{print $4}' )
+	match_end=$( cat ${out} | grep ${protein} | awk '{print $5}' )
+
+
+	echo -e -n "${sequence}\t${protein}\t${match_start}\t${match_end}" >> MatchedModification.txt
+
+	for aa in $( echo ${amino_acids} | awk -F, '{ for(i=1;i<=NF;i++) print $i }' ) ; do
+		positions=$( echo ${cleaned_sequence} | grep -o . | grep -n "[${aa}]" | awk -F: -v s=${match_start} '{x=x""$1+s";"}END{printf(substr(x, 1, length(x)-1))}' )
+
+		all_modified_positions=$( echo ${sequence} | sed 's/_//g' | awk -v s=${match_start} '{ while(( m = match($0,/\(/) ) > 0 ){ x=x""s+m-1";"; sub(/\(.{2,4}\)/,"",$0); } }END{printf(substr(x, 1, length(x)-1)) }' )
+
+		
+		#	comm -12 <( echo "12;14;21" | sed 's/;/\n/g' | sort ) <( echo "2;14"| sed 's/;/\n/g' | sort )
+		#	comm is meant to compare 2 files containing sorted lists.
+		#		-12 means ignore items only in 1 of those lists, leaving just those that are in both.
+
+		modified_positions=$( comm -12 <( echo ${positions} | sed 's/;/\n/g' | sort ) <( echo ${all_modified_positions} | sed 's/;/\n/g' | sort ) )
+
+
+		echo -e -n "\t${positions}\t${modified_positions}" >> MatchedModification.txt
+
+	done
+
+	echo >> MatchedModification.txt
+
+
+
+
+done < evidence.${acids}.txt
+#done < <( head evidence.${acids}.txt )
+
+echo 'Done'
 

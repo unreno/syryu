b43dd7823b22c64781cb22a51317e2285d286c8d
Jake <github@jakewendt.com>
2018-07-09 16:32:54 -0700

    Deving bulk msconvert/moda script.

--- archive/msconvert_moda.rb
+++ archive/msconvert_moda.rb
@@ -2,13 +2,13 @@
 
 require 'fileutils'
 
-#	running on D:
+#	running on D: as has more space
 
 source  = 'D:\massive.ucsd.edu\MSV000079053'
 
-out_base = 'D:\out'
+target = 'D:\out'
 
-FileUtils.mkdir_p "#{out_base}" unless File.directory? "#{out_base}"
+FileUtils.mkdir_p "#{target}" unless File.directory? "#{target}"
 
 
 #msconvert = 'C:\Program Files\ProteoWizard\ProteoWizard 3.0.18187.b51377ef8\msconvert.exe';
@@ -20,9 +20,7 @@ bacterium = ARGV[0]
 
 
 puts bacterium
-out_bacterium = "#{out_base}\\#{bacterium}"
-FileUtils.mkdir_p "#{out_bacterium}" unless File.directory? "#{out_bacterium}"
-
+FileUtils.mkdir_p "#{target}\\#{bacterium}" unless File.directory? "#{target}\\#{bacterium}"
 
 Dir::chdir( "#{source}\\raw\\#{bacterium}" )
 
@@ -32,28 +30,29 @@ Dir["*"].each do |raw|
 	puts "-Running msconvert on #{raw}"
 
 	#	lots of quotes are needed
-	puts "\"#{msconvert}\" #{raw} --mgf --filter \"msLevel 2\" --filter \"zeroSample removeExtra\" --outdir \"#{out_bacterium}\"";
-	puts `"#{msconvert}" #{raw} --mgf --filter "msLevel 2" --filter "zeroSample removeExtra" --outdir "#{out_bacterium}"`;
+	puts "\"#{msconvert}\" #{raw} --mgf --filter \"msLevel 2\" --filter \"zeroSample removeExtra\" --outdir \"#{target}\\#{bacterium}\"";
+	puts `"#{msconvert}" #{raw} --mgf --filter "msLevel 2" --filter "zeroSample removeExtra" --outdir "#{target}\\#{bacterium}"`;
 
-	mgf = raw.gsub(/RAW$/,"mgf")
-	out_mgf = "#{out_bacterium}\\#{mgf}"
+	raw_base = raw.gsub(/\.RAW$/,"")
+	mgf = "#{raw_base}.mgf")
 
 	puts "#{source}\\raw\\#{bacterium}\\#{raw}"
 #	File.delete "#{source}\\raw\\#{bacterium}\\#{raw}"
 
 	Dir::chdir( "#{source}\\sequence\\#{bacterium}" )
-	Dir["*"].each do |sequence|
+	Dir["*"].each do |fasta|
 
-		outdir = "#{out_bacterium}\\#{sequence.gsub(/\.fasta$/,'')}"
+		fasta_base = fasta.gsub(/\.fasta$/,'')
+		outdir = "#{target}\\#{bacterium}\\#{fasta_base}"
 		FileUtils.mkdir_p "#{outdir}" unless File.directory? "#{outdir}"
 		Dir::chdir "#{outdir}"
 
 		puts "--Creating config"
-		config = mgf.gsub(/mgf$/,"config")
+		config = "#{raw_base}.config")
 		File.open(config,"w") do |f|
 
-			f.puts "Spectra=#{out_mgf}"
-			f.puts "Fasta=#{source}\\sequence\\#{bacterium}\\#{sequence}"
+			f.puts "Spectra=#{target}\\#{bacterium}\\#{mgf}"
+			f.puts "Fasta=#{source}\\sequence\\#{bacterium}\\#{fasta}"
 
 			f.puts "Instrument=ESI-TRAP"
 			f.puts "PeptTolerance=0.5"
@@ -68,22 +67,22 @@ Dir["*"].each do |raw|
 			f.puts "HighResolution=OFF"
 		end
 
-		puts "--Running MODa on #{raw} and #{sequence}"
+		puts "--Running MODa on #{raw} and #{fasta}"
 
-		out = "#{outdir}/#{mgf.gsub(/mgf$/,"out")}"
+		out = "#{outdir}/#{raw_base}.out"
 		puts "Running java -Xmx10G -jar C:\\ryulab\\moda_v1.51\\moda_v151.jar -i \"#{config}\" -o \"#{out}\""
 		puts `java -Xmx10G -jar C:\\ryulab\\moda_v1.51\\moda_v151.jar -i "#{config}" -o "#{out}"`
 
 		puts "gzip #{out}"
 		`gzip --best #{out}`
 
-#		puts "#{source}/sequence/#{bacterium}/#{sequence}"
-#		File.delete("#{source}/sequence/#{bacterium}/#{sequence}")
+#		puts "#{source}/sequence/#{bacterium}/#{fasta}"
+#		File.delete("#{source}/sequence/#{bacterium}/#{fasta}")
 
 	end
 
-	puts "gzip #{out_mgf}"
-	`gzip --best #{out_mgf}`
+	puts "gzip #{target}\\#{bacterium}\\#{mgf}"
+	`gzip --best #{target}\\#{bacterium}\\#{mgf}`
 
 end
 
